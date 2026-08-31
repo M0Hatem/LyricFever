92aac0f17de17612b44857de5316a1aa2ea66e58
M16 <moh.medh.tem1@gmail.com>
2026-08-31 09:39:15 +0300

    fix(build): initialize MediaController without bundle identifier

--- LyricFever/ViewModel.swift
+++ LyricFever/ViewModel.swift
@@ -24,7 +24,7 @@ import MediaRemoteAdapter
     static let shared = ViewModel()
     
     // Apple Music Tahoe broken AppleScript workaround
-    let musicController = MediaController(bundleIdentifier: "com.apple.Music")
+    let musicController = MediaController()
 //    var appleMusicUniqueIdentifier: String?
 
     var currentlyPlaying: String?
